d14e7e9115a2869cdf6fd7b99cf580361c194ef4
BHAVISHEK1 <161573600+BHAVISHEK1@users.noreply.github.com>
2024-05-27 19:00:32 +0530

    s

--- src/ShowAllPatients.js
+++ src/ShowAllPatients.js
@@ -34,10 +34,6 @@ const ShowAllPatients = () => {
         setIsDetailsModalOpen(true);
     };
 
-
-
-
-    
     const handleDelete = async (patientId, patientFirstName) => {
         const confirmDelete = window.confirm(`Are you sure you want to delete ${patientFirstName}?`);
         if (confirmDelete) {
@@ -45,7 +41,6 @@ const ShowAllPatients = () => {
                 await axios.delete(`https://clinic-backend-4.onrender.com/api/patients/${patientId}`);
                 setAllPatients(allPatients.filter(patient => patient._id !== patientId));
                 toast.success(`Patient ${patientFirstName} deleted successfully`);
-                 // Close details modal after successful deletion
             } catch (error) {
                 console.error('Error deleting patient:', error);
                 toast.error('Error deleting patient. Please try again later.');
@@ -55,11 +50,6 @@ const ShowAllPatients = () => {
         }
         closeDetailsModal();
     };
-    
-
-
-
-
 
     const handleEditClick = (patient) => {
         setEditingPatient(patient);
@@ -92,14 +82,32 @@ const ShowAllPatients = () => {
         return formattedDate;
     };
 
+    const handleExportCSV = async () => {
+        try {
+            const response = await axios.get('https://clinic-backend-4.onrender.com/api/patients/export/csv', { responseType: 'blob' });
+            const url = window.URL.createObjectURL(new Blob([response.data]));
+            const link = document.createElement('a');
+            link.href = url;
+            link.setAttribute('download', 'patients.csv');
+            document.body.appendChild(link);
+            link.click();
+            toast.success('CSV file downloaded successfully');
+        } catch (error) {
+            console.error('Error exporting to CSV:', error);
+            toast.error('Error exporting to CSV. Please try again later.');
+        }
+    };
+
     return (
         <div id="showallpatients">
+            
             {allPatients.length === 0 ? (
                 <p>Patient Database is empty</p>
             ) : (
                 <>
                     <p>{allPatients.length} Entries of patients</p>
                     <p>Click on Patient name to see details</p>
+                    <button onClick={handleExportCSV}>Export to CSV</button>
                     <ul>
                         {allPatients.map(patient => (
                             <li key={patient._id}>
@@ -162,3 +170,4 @@ const ShowAllPatients = () => {
 };
 
 export default ShowAllPatients;
+
